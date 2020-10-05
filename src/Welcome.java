80e2e9dc8af74df3cd3fa417c29508c921bcadaa
Michal Zygor <michal@winami.com>
2020-10-05 19:34:57 +0200

    dodanie klasy Welcome

---
+++ src/Welcome.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Welcome {
+    public static void main(String[] args) {
+        String firstName ="Józef";
+        String lastName = "Kobzik";
+        System.out.println("Cześć" + firstName);
+        System.out.print("W naszej bzie istnieje " + firstName + " " + lastName);
+
+
+
+
+
+
+    }
+}
